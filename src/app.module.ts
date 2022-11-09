4a723eafcc4b5f3b72a862adc9ed6bd4ba491135
tomasmat12 <54420035+tomasmat12@users.noreply.github.com>
2022-11-09 17:26:16 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,22 @@ import { Producto } from './productos/entities/producto.entity';
 import { ProductosModule } from './productos/productos.module';
 import { OfertasModule } from './ofertas/ofertas.module';
 import { Oferta } from './ofertas/entities/oferta.entity';
+//New
+const db_options = {
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432'),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME, 
+}
+
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      url: process.env.DATABASE_URL,
+      ...db_options, //New
+      //url: process.env.DATABASE_URL,
       entities: [Producto, Oferta],
       synchronize: true,
       ssl: {
